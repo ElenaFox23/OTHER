77b72c39120ca5417ff55b1b26593e5911eddbdd
Elena <elenam23@mail.ru>
2022-11-27 23:14:21 +0500

    выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine($"Случайное число: {n}");
+if (n1 > n2)
+Console.Write(n1);
+else
+Console.Write(n2);
