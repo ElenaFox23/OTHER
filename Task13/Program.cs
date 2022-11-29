4266d55146e278e21165e4daff01ff5c1dc3e62f
Elena <elenam23@mail.ru>
2022-11-29 13:35:50 +0500

    Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число; ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0) n = n * -1;
+else
+if (n < 100) 
+    Console.WriteLine("Нет третьей цифры ");
+else
+while (n > 1000)
+    n /=10;
+
+Console.WriteLine(n % 10);
